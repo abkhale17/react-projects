1bc186adfe4058df5b9fdf07cb81e6f6fea786b9
abkhale <abkhale.17@gmail.com>
2024-02-09 12:24:58 +0530

    added step functionality

--- src/steps/App.js
+++ src/steps/App.js
@@ -1,23 +1,43 @@
+import { useState } from "react"
 import "../index.css"
 import "./App.css"
 
 function App() {
+  const [stepNumber, setStepNumber] = useState(2)
+  const stepDetails = [
+    {
+      stepNumber: 1,
+      content: "Step 1"
+    },
+    {
+      stepNumber: 2,
+      content: "Step 2"
+    },
+    {
+      stepNumber: 3,
+      content: "Step 3"
+    },
+  ]
   return (
     <div className="container">
       <section className="step-numbers">
-        <p className="step-number">1</p>
-        <p className="step-number">2</p>
-        <p className="step-number">3</p>
+        {stepDetails.map((step, key) => <Step key={key} stepNumber={step.stepNumber} currentStepNumber={stepNumber} />)}
       </section>
       <section className="step-content">
-        Step N: This is content section
+        { stepDetails.find(step => step.stepNumber === stepNumber)?.content }
       </section>
       <section className="step-actions">
-        <button className="btn">Previos</button>
-        <button className="btn">Next</button>
+        <button className="btn" onClick={() => setStepNumber(stepNumber-1)} disabled={stepNumber <= 1}>Previos</button>
+        <button className="btn" onClick={() => setStepNumber(stepNumber+1)} disabled={stepNumber >= 3}>Next</button>
       </section>
     </div>
   )
+}
+
+function Step({ stepNumber, currentStepNumber }) {
+  return stepNumber <= currentStepNumber 
+    ? <p className="step-number step-completed">{stepNumber}</p>
+    : <p className="step-number">{stepNumber}</p> 
 }
 
 export default App
