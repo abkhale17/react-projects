859769c7a11286acce19323ed8600641556205c3
abkhale <abkhale.17@gmail.com>
2024-02-10 19:49:25 +0530

    add close btn

--- src/steps/App.js
+++ src/steps/App.js
@@ -2,42 +2,49 @@ import { useState } from "react"
 import "../index.css"
 import "./App.css"
 
+const stepDetails = [
+  {
+    stepNumber: 1,
+    content: "Step 1"
+  },
+  {
+    stepNumber: 2,
+    content: "Step 2"
+  },
+  {
+    stepNumber: 3,
+    content: "Step 3"
+  },
+]
+
 function App() {
   const [stepNumber, setStepNumber] = useState(2)
-  const stepDetails = [
-    {
-      stepNumber: 1,
-      content: "Step 1"
-    },
-    {
-      stepNumber: 2,
-      content: "Step 2"
-    },
-    {
-      stepNumber: 3,
-      content: "Step 3"
-    },
-  ]
+  const [open, setOpen] = useState(true)
+
   return (
-    <div className="container">
-      <section className="step-numbers">
-        {stepDetails.map((step, key) => <Step key={key} stepNumber={step.stepNumber} currentStepNumber={stepNumber} />)}
-      </section>
-      <section className="step-content">
-        { stepDetails.find(step => step.stepNumber === stepNumber)?.content }
-      </section>
-      <section className="step-actions">
-        <button className="btn" onClick={() => setStepNumber(stepNumber-1)} disabled={stepNumber <= 1}>Previos</button>
-        <button className="btn" onClick={() => setStepNumber(stepNumber+1)} disabled={stepNumber >= 3}>Next</button>
-      </section>
+    <div>
+      <div className="close-btn" onClick={() => setOpen(!open)}>&times;</div>
+      {open && (
+        <div className="steps">
+          <section className="step-numbers">
+            {stepDetails.map((step, key) => <Step key={key} stepNumber={step.stepNumber} currentStepNumber={stepNumber} />)}
+          </section>
+          <section className="step-content">
+            { stepDetails.find(step => step.stepNumber === stepNumber)?.content }
+          </section>
+          <section className="step-actions">
+            <button className="btn" onClick={() => setStepNumber(stepNumber-1)} disabled={stepNumber <= 1}>Previos</button>
+            <button className="btn" onClick={() => setStepNumber(stepNumber+1)} disabled={stepNumber >= 3}>Next</button>
+          </section>
+        </div>
+      )}
+      
     </div>
   )
 }
 
 function Step({ stepNumber, currentStepNumber }) {
-  return stepNumber <= currentStepNumber 
-    ? <p className="step-number step-completed">{stepNumber}</p>
-    : <p className="step-number">{stepNumber}</p> 
+  return <p className={stepNumber <= currentStepNumber ? "step-number step-completed" : "step-number"}>{stepNumber}</p>
 }
 
 export default App
